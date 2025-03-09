eae203b134c0d60c3fcf8c8055d96ca0d53befba
denis_stepanok <elfdenis@gmail.com>
2025-03-09 17:33:57 +0200

    start

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -11,6 +11,7 @@ const refs = {
   sliderContainer: document.querySelector('.swiper-wrapper-reviews'),
   slider: document.querySelector('.swiper-reviews'),
   reviewsSection: document.querySelector('#reviews'),
+  btnBlock: document.querySelector('.btn-wrapper-reviews'),
 };
 
 const BASE_URL = 'https://portfolio-js.b.goit.study/api';
@@ -29,6 +30,7 @@ const fetchReviews = async () => {
 const renderError = () => {
   refs.sliderContainer.innerHTML = `<li class="swiper-placeholder">Not found</li>`;
   refs.slider.classList.remove('swiper-reviews');
+  refs.btnBlock.classList.add('hidden-btn');
 };
 
 const errorToast = () => {
